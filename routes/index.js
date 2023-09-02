2544392b261c5bf766963d74e2d80e01ca287b6a
Your Name <you@example.com>
2023-09-02 17:37:42 +1000

    Initial Project Commit

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,25 @@ var express = require('express');
 var router = express.Router();
 
 const nameList = [
-{ 'id': 1, 'name': 'john', 'email': 'aaa'},
-{ 'id': 2, 'name': 'james', 'email': 'dd'},
-{ 'id': 100, 'name': 'david', 'email': 'nn'},
+{ 'id': 0, 'Term': 'john', 'Description': 'aaa' , 'Reference': 'bbb'},
+{ 'id': 1, 'Term': 'john', 'Description': 'aaa' , 'Reference': 'bbb'},
+{ 'id': 2, 'Term': 'john', 'Description': 'aaa' , 'Reference': 'bbb'},
+{ 'id': 3, 'Term': 'john', 'Description': 'aaa' , 'Reference': 'bbb'},
+{ 'id': 4, 'Term': 'john', 'Description': 'aaa' , 'Reference': 'bbb'},
+{ 'id': 5, 'Term': 'john', 'Description': 'aaa' , 'Reference': 'bbb'},
+{ 'id': 6, 'Term': 'john', 'Description': 'aaa' , 'Reference': 'bbb'},
+{ 'id': 7, 'Term': 'john', 'Description': 'aaa' , 'Reference': 'bbb'},
+{ 'id': 8, 'Term': 'john', 'Description': 'aaa' , 'Reference': 'bbb'},
+{ 'id': 9, 'Term': 'john', 'Description': 'aaa' , 'Reference': 'bbb'},
+{ 'id': 10, 'Term': 'john', 'Description': 'aaa' , 'Reference': 'bbb'},
+
 ];
 
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'SIT722', sub: 'abc', names: nameList  });
+  res.render('index', { title: 'SIT722 DevOps Glossary', sub: 'my website showing a collection of DevOps terms and their brief description', names: nameList  });
 });
 
 module.exports = router;
